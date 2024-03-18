4f8ab1348e99d471d14fd7e0a2543a1505e624d9
Miroslav Hudak <miroslav.hudak@siemens-healthineers.com>
2024-03-18 15:24:44 +0100

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -29,6 +29,9 @@ switch ($command) {
     "test" {
         go test -v ./...
     }
+    "docker" {
+        docker build -t hudakmshs/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     "mongo" {
         mongo up
     }
